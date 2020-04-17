525bbedc4474c080c821e28506a71efa630faeae
Christoph Reimers <christoph@superservice-international.com>
2020-04-17 11:57:01 +0200

    add client

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 - create a session called `sm400` via the API
 - add an rstp stream to the session via the API
 - make sure your browser accepts the TLS certificate by navigating to `local_ip:4443`
-- run `yarn start` and open your browser at `local_ip:3000`
+- run `cd client`, `yarn install` and `yarn start`, then open your browser at `local_ip:3000`
---
+++ client/src/App.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect, useRef, useMemo } from "react";
+// import OpenViduSession from "openvidu-react";
+import { OpenVidu } from "openvidu-browser";
+
+const UserVideoComponent = ({ streamManager }) => {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    streamManager.addVideoElement(videoRef.current);
+  }, [videoRef, streamManager]);
+
+  return <video autoPlay={true} ref={videoRef} />;
+};
+
+function App() {
+  const [token, setToken] = useState(null);
+  const [subscribers, setSubscribers] = useState([]);
+
+  const session = useMemo(() => new OpenVidu().initSession(), []);
+
+  useEffect(() => {
+    const getToken = async () => {
+      const response = await fetch(
+        `https://${window.location.hostname}:4443/api/tokens`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: "Basic " + btoa("OPENVIDUAPP:MY_SECRET"),
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ session: "sm400", role: "SUBSCRIBER" }),
+        }
+      );
+      const json = await response.json();
+      setToken(json.token);
+    };
+    getToken();
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      console.log("token", token);
+      const themSubscribers = [];
+      session.on("streamCreated", (e) => {
+        const subscriber = session.subscribe(e.stream, undefined);
+        themSubscribers.push(subscriber);
+        setSubscribers(themSubscribers);
+      });
+      session.connect(token);
+    }
+  }, [token]);
+
+  const onJoinSession = (e) => {
+    console.log("join session", e);
+  };
+
+  const onLeaveSession = (e) => {
+    console.log("leave session", e);
+  };
+
+  const onError = (e) => {
+    console.log("error", e);
+  };
+
+  if (!token) {
+    return null;
+  }
+
+  return (
+    <div>
+      {subscribers.map((sub, i) => {
+        return (
+          <div key={`sub-${i}`}>
+            <UserVideoComponent streamManager={sub} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
